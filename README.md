023f2762b3dceb03403361dc97c377844131eeb8
Shinernd <vnflqkq@gmail.com>
2019-09-13 21:39:57 -0400

    Three Empty Triangles

--- README.md
+++ README.md
@@ -287,7 +287,6 @@ It works really fast now.
 <br>
 ## Day 4 (190907)
 * Completed the wireframe rendering
-* Reversed the code
 <br>
 
 ### Wireframe Rendering
@@ -406,4 +405,77 @@ if (!line.compare(0, 2, "v ")) {
 ```
 I'm guessing that it means if it is v, push to verts_, else if it is f, push to faces_. I wish I could open the object file as code so that I can figure out what it means.
 <br>
+<br>
 Other comments are in the code.
+<br>
+<br>
+<br>
+
+## Day 5 (190909)
+* Filled triangles
+<br>
+
+### Filling Triangles
+<br>
+
+#### 1. I got the code for making three empty triangles.
+```cpp
+// main.cpp
+#include <vector>
+#include <cmath>
+#include "tgaimage.hpp"
+#include "geometry.h"
+
+const TGAColor white = TGAColor(255, 255, 255, 255);
+const TGAColor red   = TGAColor(255, 0,   0,   255);
+const TGAColor green = TGAColor(0,   255, 0,   255);
+const int width  = 200;
+const int height = 200;
+
+void line(Vec2i p0, Vec2i p1, TGAImage &image, TGAColor color) {
+    bool steep = false;
+    if (std::abs(p0.x-p1.x)<std::abs(p0.y-p1.y)) {
+        std::swap(p0.x, p0.y);
+        std::swap(p1.x, p1.y);
+        steep = true;
+    }
+    if (p0.x>p1.x) {
+        std::swap(p0, p1);
+    }
+
+    for (int x=p0.x; x<=p1.x; x++) {
+        float t = (x-p0.x)/(float)(p1.x-p0.x);
+        int y = p0.y*(1.-t) + p1.y*t;
+        if (steep) {
+            image.set(y, x, color);
+        } else {
+            image.set(x, y, color);
+        }
+    }
+}
+
+void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color) {
+    line(t0, t1, image, color);
+    line(t1, t2, image, color);
+    line(t2, t0, image, color);
+}
+
+int main(int argc, char** argv) {
+    TGAImage image(width, height, TGAImage::RGB);
+
+    Vec2i t0[3] = {Vec2i(10, 70),   Vec2i(50, 160),  Vec2i(70, 80)};
+    Vec2i t1[3] = {Vec2i(180, 50),  Vec2i(150, 1),   Vec2i(70, 180)};
+    Vec2i t2[3] = {Vec2i(180, 150), Vec2i(120, 160), Vec2i(130, 180)};
+
+    triangle(t0[0], t0[1], t0[2], image, red);
+    triangle(t1[0], t1[1], t1[2], image, white);
+    triangle(t2[0], t2[1], t2[2], image, green);
+
+
+    image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
+    image.write_tga_file("output.tga");
+    return 0;
+}
+```
+The output of the code is like below.
+![emptytriangles](https://ifh.cc/g/9pDBr.jpg)
--- main.cpp
+++ main.cpp
@@ -9,82 +9,55 @@
 #include <vector>
 #include <cmath>
 #include "tgaimage.hpp"
-#include "model.h"
 #include "geometry.h"
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
+const TGAColor green = TGAColor(0,   255, 0,   255);
+const int width  = 200;
+const int height = 200;
 
-Model *model = NULL;  // model.cpp model.h
-
-const int width = 800;
-const int height = 800;
-
-void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
+void line(Vec2i p0, Vec2i p1, TGAImage &image, TGAColor color) {
     bool steep = false;
-    
-    if (std::abs(x1-x0) < std::abs(y1-y0))
-    {
-        std::swap(x0, y0);
-        std::swap(x1, y1);
+    if (std::abs(p0.x-p1.x)<std::abs(p0.y-p1.y)) {
+        std::swap(p0.x, p0.y);
+        std::swap(p1.x, p1.y);
         steep = true;
     }
-    
-    if (x0 > x1)
-    {
-        std::swap(x0, x1);
-        std::swap(y0, y1);
+    if (p0.x>p1.x) {
+        std::swap(p0, p1);
     }
-
-//    int dx = x1-x0;
-//    int dy = y1-y0;
-//    int derror = std::abs(dy)*2;
-//
-//    float error = 0;
-//    int y = y0;
     
-    for (int x=x0; x<=x1; x++) {
-        float t = (x-x0)/(float)(x1-x0);
-        int y = y0 * (1.-t) + y1 * t;
+    for (int x=p0.x; x<=p1.x; x++) {
+        float t = (x-p0.x)/(float)(p1.x-p0.x);
+        int y = p0.y*(1.-t) + p1.y*t;
         if (steep) {
             image.set(y, x, color);
         } else {
             image.set(x, y, color);
         }
-//        error += derror;
-//        if (error > dx) {
-//            y += (y1>y0?1:-1);
-//            error -= dx*2;
-//        }
     }
-    
 }
 
-int main(int argc, char** argv) {                   // Q: How can main() have parameters?
-    if (argc==2) {                                  // the first parameter is used
-        model = new Model(argv[1]);                 // the second parameter is used
-    } else {
-        model = new Model("obj/african_head.obj");  // model.cpp - line 8: Model::Model(const char *filename) : ...
-    }
-    
+void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color) {
+    line(t0, t1, image, color);
+    line(t1, t2, image, color);
+    line(t2, t0, image, color);
+}
+
+int main(int argc, char** argv) {
     TGAImage image(width, height, TGAImage::RGB);
     
-    for (int i=0; i<model->nfaces(); i++) {
-        std::vector<int> face = model->face(i);     // face = faces_[i]
-        for (int j=0; j<3; j++) {
-            Vec3f v0 = model->vert(face[j]);        // v0 = verts_[faces_[j]]
-            Vec3f v1 = model->vert(face[(j+1)%3]);  // v1 = verts_[faces_[(j+1)%3]);
-            int x0 = (v0.x + 1.) * width / 2.;
-            int y0 = (v0.y + 1.) * height / 2.;
-            int x1 = (v1.x + 1.) * width / 2.;
-            int y1 = (v1.y + 1.) * height / 2.;
-            line(x0, y0, x1, y1, image, white);
-        }
-    }
+    Vec2i t0[3] = {Vec2i(10, 70),   Vec2i(50, 160),  Vec2i(70, 80)};
+    Vec2i t1[3] = {Vec2i(180, 50),  Vec2i(150, 1),   Vec2i(70, 180)};
+    Vec2i t2[3] = {Vec2i(180, 150), Vec2i(120, 160), Vec2i(130, 180)};
     
-    image.flip_vertically();
+    triangle(t0[0], t0[1], t0[2], image, red);
+    triangle(t1[0], t1[1], t1[2], image, white);
+    triangle(t2[0], t2[1], t2[2], image, green);
+    
+    
+    image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
     image.write_tga_file("output.tga");
-    
-    delete model;                                   // delete the object
     return 0;
 }
